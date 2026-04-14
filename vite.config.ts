e8c6280c720620d7cc1cb04278ad126f60bfbbb1
Yuvarajapathi278 <yuvarajapathi278@gmail.com>
2026-04-15 00:30:05 +0530

    remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Yuvarajapathi-s-Portfolio/', // Add this line for GitHub Pages
   server: {
     port: 5173,
     open: true
